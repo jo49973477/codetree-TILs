31a80f764e2babc912faa9b9e8f14cfa02cc8dbb
jo49973477 <67433199+jo49973477@users.noreply.github.com>
2024-07-16 20:25:48 +0900

    Title: 금 채굴하기, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240716/금 채굴하기/gold-mining.py
@@ -0,0 +1,27 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split()) # N*N줄 , 가격 M원
+field = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+def gold_n_cost(dot, K, f):
+    x, y = dot
+    dots_square = [(x+i, y+j) for i in range(-K, K+1) for j in range(-K+abs(i), K-abs(i)+1)]
+    
+    ans = 0
+    cost = 0
+    for (i, j) in dots_square:
+        if 0 <= i <= N-1 and 0 <= j <= N-1:
+            ans += f[i][j]
+            cost += 1
+            
+    return ans, cost
+
+max_gold = 0
+
+for i in range(N):
+    for j in range(N):
+        for k in range(N//2+1):
+            gold, cost = gold_n_cost((i, j), k, field)
+            max_gold = max((max_gold, gold))if (M*gold - cost)>= 0 else max_gold
+
+print(max_gold)
