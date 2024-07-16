d699c2430ad7d80544c152c1d121c6d4db9aff3b
jo49973477 <67433199+jo49973477@users.noreply.github.com>
2024-07-16 19:05:08 +0900

    Title: 트로미노, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240716/트로미노/tromino.py
@@ -0,0 +1,20 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split()) # M열 N줄
+f = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+ans = 0
+for i in range(N-1):
+    for j in range(M-1):
+        ans = max([ans, f[i][j] + f[i+1][j] + f[i][j+1], f[i][j] + f[i+1][j] + f[i+1][j+1],
+                   f[i][j] + f[i][j+1] + f[i+1][j+1], f[i][j]+1 + f[i+1][j] + f[i+1][j+1]])
+
+for i in range(N):
+    for j in range(M-2):
+        ans = max([ans, f[i][j] + f[i][j+1] + f[i][j+2]])
+        
+for i in range(N-2):
+    for j in range(M):
+        ans = max([ans, f[i][j] + f[i+1][j] + f[i+2][j]])
+        
+print(ans)
