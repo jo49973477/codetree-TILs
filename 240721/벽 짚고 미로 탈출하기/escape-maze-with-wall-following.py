3c9ce72931fb5226f28e60177f1432f40d55569c
jo49973477 <67433199+jo49973477@users.noreply.github.com>
2024-07-21 03:12:31 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240721/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -0,0 +1,34 @@
+import sys
+
+N = int(sys.stdin.readline())
+x, y = map(int, sys.stdin.readline().split())
+field = [sys.stdin.readline().strip() for _ in range(N)]
+
+
+x, y = x-1, y-1
+initial_x, initial_y = x, y
+dxs, dys = [-1, 0, 1, 0] , [0, -1, 0, 1]
+
+idx = 2
+T = 0 
+while True:
+    dx, dy = dxs[idx], dys[idx] #앞으로갈길
+    bx, by = dxs[(idx+1)%4], dys[(idx+1)%4] #바닥위치
+    
+    if T > 0 and x == initial_x and y == initial_y:
+        T = -1
+        break
+    elif 0 <= x <= N-1 and 0 <= y <= N-1:
+        if 0 <= x+dx <= N-1 and 0 <= y+dy <= N-1 and field[y+dy][x+dx] == '#': #막히면
+            idx = idx - 1 if idx != 0 else 3 #방향틀기
+        elif field[y+by][x+bx] == '.': #아래에 바닥없으면
+            idx = (idx+1)%4 #또 방향틀기
+            x, y = x+bx, y+by #방향틀어서 전진
+            T += 1
+        elif field[y+by][x+bx] == '#': #아래에 바닥이 있다?
+            x, y = x+dx, y+dy
+            T += 1 #전진!
+    else:
+        break
+
+print(T)
