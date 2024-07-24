ebd4a89cb06f45c6530bfede80cebeb28c8e3de2
jo49973477 <67433199+jo49973477@users.noreply.github.com>
2024-07-24 21:00:45 +0900

    Title: 겹치지 않게 선분 고르기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240724/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -0,0 +1,26 @@
+import sys
+
+ans = 0
+
+def backtrack(lines, line_num, last):
+    global ans
+    
+    if not lines:
+        ans = max((ans, line_num))
+    else:
+        isObservable = False
+        for i, (x1, x2) in enumerate(lines):
+            if x1 > last:
+                isObservable = True
+                backtrack(lines[(i+1):], line_num + 1, x2)
+            else:
+                continue
+        if not isObservable:
+            ans = max((ans, line_num))            
+
+N = int(sys.stdin.readline())
+lines = [tuple(map(int, sys.stdin.readline().split())) for _ in range(N)]
+lines.sort(key= lambda x: x[0])
+
+backtrack(lines, 1, lines[0][0])
+print(ans)
