1f44c297d5eb633ed1c9e78e5fc00780b13ed0a8
jo49973477 <67433199+jo49973477@users.noreply.github.com>
2024-07-24 19:15:06 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 270ms, Memory: 28MB, Status: Passed - CodeTree

--- 240724/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
+++ 240724/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
@@ -7,10 +7,10 @@ def backtracking(depth,K, N, result):
     else:
         for num in range(1, K+1):
             result[depth] = num
-            backtracking(depth + 1, N, result)
+            backtracking(depth+1, K, N, result)
             result[depth] = 0
 
-K, N = map(int, sys.stdin.readline())
+K, N = map(int, sys.stdin.readline().split())
 
 result = [0 for _ in range(N)]
 backtracking(0, K, N, result)
