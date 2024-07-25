ffe2aec0988ba052fd22b0fd8059118763d2f3c1
jo49973477 <67433199+jo49973477@users.noreply.github.com>
2024-07-26 00:21:01 +0900

    Title: 최소 점프 횟수, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240726/최소 점프 횟수/min-num-of-jumps.py
+++ 240726/최소 점프 횟수/min-num-of-jumps.py
@@ -4,13 +4,17 @@ N = int(sys.stdin.readline())
 leg_list = list(map(int, sys.stdin.readline().split()))
 
 ans = sys.maxsize
+isMoved = False
 def backtrack(pos, moved):
     if pos >= N-1:
         global ans
         ans = min((ans, moved))
+
+        global isMoved
+        isMoved = True
     else:
         for mv in range(1, leg_list[pos]+1):
             backtrack(pos + mv, moved + 1)
 
 backtrack(0, 0)
-print(ans)
+print(ans if isMoved else -1)
