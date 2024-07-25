996d198184458a7da7e113a0b063f7ff3b2b4550
jo49973477 <67433199+jo49973477@users.noreply.github.com>
2024-07-25 21:46:47 +0900

    Title: 1차원 윷놀이, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240725/1차원 윷놀이/yutnori-1d.py
@@ -0,0 +1,24 @@
+import sys
+
+N, M, K = map(int, sys.stdin.readline().split()) # K개 말, M개 판
+yut_list = list(map(int, sys.stdin.readline().split())) # N개 윷
+
+ans = 0
+
+def backtrack(depth, pos_list):
+    if depth == N:
+        arrived = sum([1 if pos >= M else 0 for pos in pos_list])
+        global ans
+        ans = max((ans, arrived))
+    else:
+        for mal in range(K):
+            if pos_list[mal] >= M:
+                continue
+            else:
+                pos_list[mal] += yut_list[depth]
+                backtrack(depth+1, pos_list)
+                pos_list[mal] -= yut_list[depth]
+                
+
+backtrack(0, [1 for _ in range(K)])
+print(ans)
