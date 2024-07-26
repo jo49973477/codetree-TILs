ba0dfb31de2740f8b2e6bd0602c65f2177b78f79
jo49973477 <67433199+jo49973477@users.noreply.github.com>
2024-07-27 00:42:02 +0900

    Title: 외판원 순회, Time: 424ms, Memory: 28MB, Status: Passed - CodeTree

--- 240727/외판원 순회/traveling-salesman-problem.py
+++ 240727/외판원 순회/traveling-salesman-problem.py
@@ -10,18 +10,18 @@ min_cost = sys.maxsize
 
 def permu(depth, visited, arr):
     if depth == N-1:
-        arr1 = copy.deepcopy(arr)
-        arr1.append(0)
-        _from = arr1[-1]
+        arr.append(0)
+        _from = arr[-1]
         cost = 0
-        for _to in arr1:
+        for _to in arr:
             if costs[_from][_to] == 0:
                 cost = -1
                 break
             else:
                 cost += costs[_from][_to]
                 _from = _to
-        
+        arr.pop()
+
         if cost != -1:
             global min_cost
             min_cost = min((min_cost, cost))
