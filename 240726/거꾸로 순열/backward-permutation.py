bfd7186c9853c76c1415bc69726d9249e3db822c
jo49973477 <67433199+jo49973477@users.noreply.github.com>
2024-07-26 23:51:21 +0900

    Title: 거꾸로 순열, Time: 201ms, Memory: 28MB, Status: Passed - CodeTree

---
+++ 240726/거꾸로 순열/backward-permutation.py
@@ -0,0 +1,18 @@
+import sys
+
+def permu(depth, N, visited, arr):
+    if depth == N:
+        print(' '.join(map(str, arr)))
+        return
+    
+    for i in range(N-1, -1, -1):
+        if visited[i]:
+            continue
+        else:
+            visited[i] = True
+            arr[depth] = i + 1
+            permu(depth + 1, N, visited, arr)
+            visited[i] = False
+
+N = int(sys.stdin.readline())
+permu(0, N, [False for _ in range(N)], [0 for _ in range(N)])
