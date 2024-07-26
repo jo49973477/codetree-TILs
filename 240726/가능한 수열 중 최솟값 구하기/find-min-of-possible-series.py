9bfdfc625dabbc6653a4f01b54b4cef6e2c43b88
jo49973477 <67433199+jo49973477@users.noreply.github.com>
2024-07-26 17:34:51 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240726/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
@@ -0,0 +1,28 @@
+import sys
+
+
+def makearray(sen, N, startpoint):
+    if len(sen) == N:
+        print(sen)
+        return
+    
+    max_conlen = len(sen) // 2 if len(sen)%2 == 0 else len(sen) // 2 + 1
+    next_candidates = list(range(startpoint, 7))
+        
+    for conlen in range(1, max_conlen + 1):
+        if not next_candidates:
+            break
+        else:
+            for can in next_candidates:
+                last1 = sen[(len(sen)-conlen+1):]
+                last2 = sen[(len(sen)-2*conlen+1):(len(sen)-conlen+1)]
+                if last1 + str(can) == last2:
+                    next_candidates.remove(can)
+    
+    if next_candidates:
+        makearray(sen+str(next_candidates[0]), N, 4)
+    else:
+        makearray(sen[:-1], N, startpoint + 1)
+            
+N = int(sys.stdin.readline())
+makearray('4', N, 4)
