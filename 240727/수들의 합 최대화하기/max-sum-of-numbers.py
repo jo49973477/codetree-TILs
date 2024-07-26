b7dc27009d9e3d00d934008a5cf16562f8495d28
jo49973477 <67433199+jo49973477@users.noreply.github.com>
2024-07-27 00:03:14 +0900

    Title: 수들의 합 최대화하기, Time: 353ms, Memory: 28MB, Status: Passed - CodeTree

---
+++ 240727/수들의 합 최대화하기/max-sum-of-numbers.py
@@ -0,0 +1,26 @@
+import sys
+
+
+
+N = int(sys.stdin.readline())
+field = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+ans = 0
+def permu(depth, visited, arr):
+    if depth == N:
+        result = sum([field[i][j] for i, j in enumerate(arr)])
+        global ans
+        ans = max((ans, result))
+        return
+    
+    for i in range(N):
+        if visited[i]:
+            continue
+        else:
+            visited[i] = True
+            arr[depth] = i
+            permu(depth + 1, visited, arr)
+            visited[i] = False
+
+permu(0, [False for _ in range(N)], [0 for _ in range(N)])
+print(ans)
