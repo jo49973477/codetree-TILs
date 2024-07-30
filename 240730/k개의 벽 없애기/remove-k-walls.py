567f08deb8cc28278f16b9da815e4d2dd115ec23
jo49973477 <67433199+jo49973477@users.noreply.github.com>
2024-07-30 20:50:13 +0900

    Title: k개의 벽 없애기, Time: 165ms, Memory: 29MB, Status: Wrong Answer - CodeTree

--- 240730/k개의 벽 없애기/remove-k-walls.py
+++ 240730/k개의 벽 없애기/remove-k-walls.py
@@ -14,28 +14,31 @@ q = deque()
 q.append((r1, c1, 0))
 
 visited = [[False for j in range(N)] for i in range(N)]
+visited[r1][c1] = True
 step = [[0 for _ in range(N)] for _ in range(N)]
 
 ans = -1
 
 while q:
     y, x, walls = q.popleft()
+    print(q)
+    print(step)
     
     if (y, x) == (r2, c2):
         ans = step[y][x]
         break
         
+    if walls > K:
+        continue
         
         
     for dy, dx in zip(dys, dxs):
-        if 0 <= y+dy <= N-1 and 0 <= x+dx <= N-1 and not visited[y+dy][x+dx]:
+        if 0 <= y+dy <= N-1 and 0 <= x+dx <= N-1:
             visited[y+dy][x+dx] = True
             step[y+dy][x+dx] = step[y][x] + 1
             
-            if field[y+dy][x+dx] == 1:
-                walls += 1
-                
-            if walls <= K:
-                q.append((y+dy, x+dx, walls))
+            next_wall = walls+1 if field[y+dy][x+dx] == 1 else walls
+            
+            q.append((y+dy, x+dx, next_wall))
 
 print(ans)
