4a3f5766b35f701d4436bb9841a866670f613831
jo49973477 <67433199+jo49973477@users.noreply.github.com>
2024-08-10 16:24:12 +0900

    Title: 정수 사각형 최장 증가 수열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240810/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -0,0 +1,36 @@
+import sys
+
+
+N = int(sys.stdin.readline())
+field = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+
+def in_range(y, x):
+    return 0 <= y <= N-1 and 0 <= x <= N-1
+
+dp = [[-1 for _ in range(N)] for _ in range(N)]
+dys = [0, 0, -1, 1]
+dxs = [-1, 1, 0, 0]
+
+def count(dot):
+    global dp
+    y, x = dot
+    
+    if dp[y][x] != -1:
+        return dp[y][x]
+    
+    ans_candidates = [1]
+    for dy, dx in zip(dys, dxs):
+        if in_range(y+dy, x+dx) and field[y+dy][x+dx] > field[y][x]:
+            ans_candidates.append(1+count((y+dy, x+dx)))
+    ans = max(ans_candidates)
+    dp[y][x] = ans
+    return ans
+
+ans = 0
+for i in range(N):
+    for j in range(N):
+        val = count((i,j))
+        ans = max((ans, val))
+
+print(ans)
