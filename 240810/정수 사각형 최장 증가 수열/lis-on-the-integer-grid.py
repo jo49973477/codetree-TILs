1aff371c1221e8349b57834d37539f86cf65b225
jo49973477 <67433199+jo49973477@users.noreply.github.com>
2024-08-10 16:27:19 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 596ms, Memory: 34MB, Status: Passed - CodeTree

--- 240810/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240810/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -1,36 +1,37 @@
-import sys
-sys.setrecursionlimit(100000)
+N = int(input())
+matrix = [
+    list(map(int, input().split()))
+    for _ in range(N)
+]
 
-N = int(sys.stdin.readline())
-field = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+# dp[i][j] = (i, j) 부터 시작하는 최대 이동 거리
+dp = [[0] * N for _ in range(N)]
+
+def in_range(x, y):
+    return 0 <= x < N and 0 <= y < N
+
+def travel(x, y, move_cnt):
+    if dp[x][y]:
+        return dp[x][y]
+
+    dx = [1, 0, -1, 0]
+    dy = [0, 1, 0, -1]
+
+    max_move_cnt = 0
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+
+        if in_range(nx, ny) and matrix[nx][ny] > matrix[x][y]:
+            max_move_cnt = max(max_move_cnt, move_cnt + travel(nx, ny, move_cnt))
+
+    # print(x, y, max_move_cnt)
+    dp[x][y] = max_move_cnt
+    return max_move_cnt
 
 
-def in_range(y, x):
-    return 0 <= y <= N-1 and 0 <= x <= N-1
-
-dp = [[-1 for _ in range(N)] for _ in range(N)]
-dys = [0, 0, -1, 1]
-dxs = [-1, 1, 0, 0]
-
-def count(dot):
-    global dp
-    y, x = dot
-    
-    if dp[y][x] != -1:
-        return dp[y][x]
-    
-    ans_candidates = [1]
-    for dy, dx in zip(dys, dxs):
-        if in_range(y+dy, x+dx) and field[y+dy][x+dx] > field[y][x]:
-            ans_candidates.append(1+count((y+dy, x+dx)))
-    ans = max(ans_candidates)
-    dp[y][x] = ans
-    return ans
-
-ans = 0
 for i in range(N):
     for j in range(N):
-        val = count((i,j))
-        ans = max((ans, val))
+        if not dp[i][j]:
+            travel(i, j, 1)
 
-print(ans)
+print(max(map(max, dp))+1)
